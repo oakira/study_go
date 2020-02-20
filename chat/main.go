2adaa7d66e38485130ed49b22be41d550ada66d1
akira ogawa <oakira@outlook.com>
2020-02-21 01:44:44 +0900

    chat change

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -16,6 +17,8 @@ type templateHandler struct {
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
 	r := newRoom()
 	//root
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -23,6 +26,7 @@ func main() {
 	//start chatroom
 	go r.run()
 	//start webserver
+	log.Println("Webサーバーを開始します。ポート：", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServer:", err)
 	}
@@ -33,5 +37,5 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
