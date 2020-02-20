058ebc4c382f68b166e5b9db0a7d033393eb94e7
akira ogawa <oakira@outlook.com>
2020-02-21 02:04:40 +0900

    chat change

--- chat/main.go
+++ chat/main.go
@@ -27,7 +27,7 @@ func main() {
 	go r.run()
 	//start webserver
 	log.Println("Webサーバーを開始します。ポート：", *addr)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServer:", err)
 	}
 }
